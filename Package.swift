7a95d0e1cfec3b57c87d0bf789be94172d324600
Manikanta Nomula <c-manikanta.nomula@charter.com>
2020-02-25 01:52:45 -0700

    Added swift package manager to Publisher.

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "Publisher",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(
+            name: "Publisher",
+            targets: ["Publisher"]),
+    ],
+    targets: [
+        .target(
+            name: "Publisher",
+            dependencies: [
+                .package(
+                    url: "https://github.com/popmedic/decouple-protocols.git",
+                    .exact("0.0.2")
+                )
+            ],
+            path: "Sources"),
+    ]
+)
+
